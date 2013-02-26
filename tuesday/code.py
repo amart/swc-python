acd7f718a1471652265b43e3c310776e81bdf8da
Teresa Amar <zta@uw.edu>
2013-02-26 18:51:49 -0500

    Learning about unit tests and nose

---
+++ tuesday/code.py
@@ -0,0 +1,10 @@
+#
+
+def stuff(values):
+    result = []
+    for v in values:
+        if v > 0:
+            result.append(2*v)
+    return(result)
+
+ 
---
+++ tuesday/test_code.py
@@ -0,0 +1,11 @@
+#
+
+def test_empty():
+    assert stuff([]) == []
+
+def test_single_value():
+    assert stuff([1]) == [2]
+
+def test_negative_value():
+    assert stuff([-5]) == []
+ 
