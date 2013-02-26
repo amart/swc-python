c95d47bbac28a15023a89fd31059be321123e331
Teresa Amar <zta@uw.edu>
2013-02-26 18:58:59 -0500

    Corrections; no failed tests

--- tuesday/code.py
+++ tuesday/code.py
@@ -3,7 +3,7 @@
 def stuff(values):
     result = []
     for v in values:
-        if v > 0:
+        if v >= 0:
             result.append(2*v)
     return(result)
 
--- tuesday/test_code.py
+++ tuesday/test_code.py
@@ -11,3 +11,9 @@ def test_single_value():
 def test_negative_value():
     assert stuff([-5]) == []
  
+def test_zero_value():
+    assert stuff([0]) == [0]
+
+def test_lots_of_values():
+    assert stuff([-1,2,-3,4,5,-6,7,-8,9]) == [4,8,10,14,18]
+
