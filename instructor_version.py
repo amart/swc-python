cbb7362d3e9502152140901b0084fcdb9639ea85
Teresa Amar <zta@uw.edu>
2013-02-26 14:25:24 -0500

    Edited version of instructor code

---
+++ instructor_version.py
@@ -0,0 +1,21 @@
+# edited version of instructor's version of the count_stars.py code
+
+def get_content(the_line):
+    return(the_line.strip().split('#')[0].strip())
+
+
+def get_number(the_content):
+    return int(the_content.split(',')[1])
+
+
+source = open('stars.txt', 'r')
+
+total = 0
+
+for line in source:
+    content = get_content(line)
+    if content != '':
+        total = total + get_number(content)
+
+print total
+
