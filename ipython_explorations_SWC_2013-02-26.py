2efd2b1cd217c883f8e7110bda6ef9b23c46d2ae
Teresa Amar <zta@uw.edu>
2013-02-26 18:10:50 -0500

    Ipython (shell) explorations with function pointers and functions with list of functions as an argument

--- ipython_explorations_SWC_2013-02-26.py
+++ ipython_explorations_SWC_2013-02-26.py
@@ -29,3 +29,34 @@ def sub_one(x):
 
 pipeline([double,sub_one,triple],5)
 
+
+
+def scale(value,factor)
+    return(value * factor)
+
+def scale_half(value)
+    return(scale(value,0.5))
+
+
+# want squish(plus,[1,2,3]) is 6
+#      squish(times,[1,2,3]) is 6
+#      squish(times,[4,5,6]) is 120
+#      squish(plus,[1,-1,2,3]) is 5
+
+def plus(x,y):
+    return(x+y)
+
+def times(x,y):
+    return(x*y)
+
+
+def squish(func_arg,list_arg):
+     result = list_arg[0]
+     if len(list_arg) > 1:
+         for l in list_arg[1:]:
+             result = func_arg(result,l)
+
+     return(result)
+
+
+
